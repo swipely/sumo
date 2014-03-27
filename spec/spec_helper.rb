98b95a2776db1201f563f04937f1e7f901cda15a
Tom Hulihan <hulihan.tom159@gmail.com>
2014-03-27 19:59:02 -0400

    Add the Code Climate test reporting gem.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 $LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
 
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start unless ENV['CODECLIMATE_REPO_TOKEN'].nil?
+
 require 'rspec'
 require 'pry'
 require 'sumo'
