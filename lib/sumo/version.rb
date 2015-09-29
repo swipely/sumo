bebc870b54c0d71026e9d275be5070271fcfe05e
Tom Hulihan <hulihan.tom159@gmail.com>
2015-09-28 21:17:33 -0400

    Minor version bump v2.1.0

--- lib/sumo/version.rb
+++ lib/sumo/version.rb
@@ -1,8 +1,8 @@
 # This module holds versioning information for the gem.
 module Sumo
   MAJOR = 2
-  MINOR = 0
-  PATCH = 3
+  MINOR = 1
+  PATCH = 0
   RELEASE = nil
 
   VERSION = [MAJOR, MINOR, PATCH, RELEASE].compact.join('.')
